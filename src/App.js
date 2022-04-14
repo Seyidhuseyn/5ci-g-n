19361f99d03cbe4863b5fcbbaf7c4d733cb491db
Seyidhuseyn <seyidhuseyn02@gmail.com>
2022-04-15 02:22:21 +0400

    navbar yaradildi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <h1>Salam</h1>
+    <>
+    <Navbar />
+    </>
   );
 }
 
